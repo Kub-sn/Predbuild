c1a775a0f1dda9c50fafd1b66dc905106c1cbea3
Kub-sn <kubi.y@hotmail.de>
2023-03-06 14:47:57 +0100

    firebase removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle'
 import {MatButtonModule} from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
-import { BuildoverviewComponent } from './buildsPage/features/buildoverview/buildoverview.component';
 import { BuildsoverviewModule } from './buildsPage/features/buildoverview/buildoverview.module';
 import { BuildDetailsComponent } from './buildsPage/features/build-details/build-details.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -28,9 +23,6 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
     MatSlideToggleModule, 
     MatButtonModule,
     BuildsoverviewModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
